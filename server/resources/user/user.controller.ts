0655ac50cca743576da38347857fd852ec3070b5
Erikisak <erik.isak@icloud.com>
2022-05-19 14:57:57 +0200

    get and post for user

--- server/resources/user/user.controller.ts
+++ server/resources/user/user.controller.ts
@@ -2,10 +2,20 @@ import { NextFunction, Request, Response } from "express";
 import { userInfo } from "os";
 import { UserModel, User } from "./user.model";
 
+
+
+
 export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await UserModel.find({});
+    res.status(200).json(users);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return res.status(500).json(err.message);
+    }
+  }
   // TODO: who is allowed to use this endpoint?
-  const users = await UserModel.find({});
-  res.status(200).json(users);
+
 };
 
 export const addUser = async (
@@ -15,14 +25,23 @@ export const addUser = async (
 ) => {
   // TODO: how do we handle errors in async middlewares?
   try {
-    const user = new UserModel(req.body);
+    const user = new UserModel({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: req.body.password
+    });
+    user.isAdmin
     await user.save();
+    return res.json("new user created")
     // console.log(user);
     console.log(user.fullname);
     // const errors = user.validateSync();
     res.status(200).json(user);
-  } catch (err) {
-    next(err);
+  } catch (err: any) {
+    if  (err.code == 1100)
+      return res.status(401).json("email does already exist")
+    res.status(500).json(err.message)
   }
 };
 
@@ -30,9 +49,23 @@ export const updateUser = async (
   req: Request<{ id: string }>,
   res: Response
 ) => {
-  const user = await UserModel.findById(req.params.id).select("+password");
-  console.log(user);
-  res.status(200).json("UPDATED USER WITH ID :" + req.params.id);
+  try{
+
+    let { firstname, lastname, email, password, isAdmin } = req.body;
+    let user = await UserModel.findByIdAndUpdate(req.params.id, req.body).select("+password")
+    if (firstname) user!.firstname = firstname;
+    if (lastname) user!.lastname = lastname;
+    if (email) user!.email = email;
+    if (password) user!.password = password;
+    if (isAdmin) user!.isAdmin = isAdmin;
+    console.log(user)
+    
+    res.status(200).json("UPDATED USER WITH ID :" + req.params.id);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return res.status(500).json(err.message);
+    }
+  }
 };
 
 export const deleteUser = (req: Request, res: Response) => {
