66ac0752d5198942f8278e8781517169f13af8c1
Millie Cheung <cheungmillie9@gmail.com>
2022-05-19 10:32:13 +0200

    changed server from 3000 to 3001 as client is using 3000

--- server/server.ts
+++ server/server.ts
@@ -19,8 +19,8 @@ mongoose.connect(
       console.log(err);
     } else {
       console.log("Connection to database established.");
-      app.listen(3000, () =>
-        console.log("Server is running on http://localhost:3000/")
+      app.listen(3001, () =>
+        console.log("Server is running on http://localhost:3001/")
       );
     }
   }
