c1eaadb54e8e8e60be7f3de881a439238dff4788
Millie Cheung <cheungmillie9@gmail.com>
2022-05-19 15:54:00 +0200

    added sample functions (to be deleted)

--- server/middleware.ts
+++ server/middleware.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 
 // stop users that arent logged in
 export const secure = (req: Request, res: Response, next: NextFunction) => {
@@ -21,3 +21,35 @@ export const adminSecure = (
     res.status(403).json("You are not permitted.");
   }
 };
+
+/* SECURITY FUNCTIONS THAT WE CAN USE 
+
+const validBody = async (req: Request, res: Response, next: NextFunction) => {
+  await new UserModel(req.body).validate();
+};
+
+const adminOnly = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.session?.user?.isAdmin) {
+    throw new HttpError(403, "not permitted");
+  }
+  next();
+};
+
+const auth = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.session?.user) {
+    throw new HttpError(401, "must login");
+  }
+  next();
+};
+
+const selfOrAdmin = async (req: Request, res: Response, next: NextFunction) => {
+  const user = await UserModel.findById(req.params.id);
+  const isAdmin = req.session?.user?.isAdmin;
+  const userOwnsContent = user && user?.id === req.session?.user?.id;
+
+  if (!isAdmin && !userOwnsContent) {
+    throw new HttpError(403, "not permitted");
+  }
+  next();
+};
+*/
