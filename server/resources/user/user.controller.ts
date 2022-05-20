d84daa44fc8a7e20970a9fdb0edc0058f7c9d13b
Millie Cheung <cheungmillie9@gmail.com>
2022-05-20 13:30:56 +0200

    function for sign in / out and get cookie session. Sign in is not fully tested esp for the encrypted password. Bug found: (1) for addUser, the same email can be used for creating several accounts

--- server/resources/user/user.controller.ts
+++ server/resources/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { User, UserModel } from "./user.model";
+import bcrypt from "bcrypt";
 
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
@@ -13,11 +14,7 @@ export const getAllUsers = async (req: Request, res: Response) => {
   // TODO: who is allowed to use this endpoint?
 };
 
-export const addUser = async (
-  req: Request<{}, {}, User>,
-  res: Response,
-  next: NextFunction
-) => {
+export const addUser = async (req: Request<{}, {}, User>, res: Response) => {
   // TODO: how do we handle errors in async middlewares?
   try {
     const user = new UserModel({
@@ -31,7 +28,7 @@ export const addUser = async (
     return res.json("new user created");
     // const errors = user.validateSync();
   } catch (err: any) {
-    if (err.code == 1100)
+    if (err.code == 11000)
       return res.status(401).json("email does already exist");
     res.status(500).json(err.message);
   }
@@ -57,4 +54,39 @@ export const updateUser = async (
       return res.status(500).json(err.message);
     }
   }
+};
+
+// sign in
+export const signIn = async (req: Request, res: Response) => {
+  const user = await UserModel.findOne({ email: req.body.email }).select(
+    "+password"
+  );
+  if (!user) {
+    res.status(401).json("Email does not exist");
+  } else if (user && user.password !== req.body.password) {
+    res.status(401).json("You have entered a wrong password.");
+  } else {
+    req.session!.user = {
+      id: user.id,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    };
+    res.status(200).json("You are now logged in.");
+  }
+};
+
+// sign out
+export const signOut = async (req: Request<{ id: string }>, res: Response) => {
+  if (!req.session?.user) return res.status(403).json("You are not logged in.");
+  req.session = null;
+  res.status(200).json("You are logged out.");
+};
+
+// return the information stored in the cookie - for testing
+export const getCookieSession = async (
+  req: Request<{ id: string }>,
+  res: Response
+) => {
+  if (!req.session?.user) return res.status(401).send("You are not logged in.");
+  res.status(200).json(req.session);
 };
