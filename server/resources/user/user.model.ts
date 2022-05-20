a3b6bf3b83fe5f0e82c38779496f5dcb25293dd1
Millie Cheung <cheungmillie9@gmail.com>
2022-05-20 11:32:23 +0200

    set unique to email

--- server/resources/user/user.model.ts
+++ server/resources/user/user.model.ts
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema(
   {
     firstname: { type: String, required: true },
     lastname: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true, select: false }, // to not include password unless on request
     isAdmin: { type: Boolean, default: false },
   },
