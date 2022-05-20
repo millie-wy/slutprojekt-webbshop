5e513c05d836000077a91e2b02f4dd32d3ad0654
Millie Cheung <cheungmillie9@gmail.com>
2022-05-20 11:32:19 +0200

    adjusted and tested functions in endpoints (to be followed-up: the deliveryOption and products objects cannot be retrieved when doing "GET")

--- server/resources/order/order.controller.ts
+++ server/resources/order/order.controller.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response } from "express";
 import { User } from "../user";
 import { Order, OrderModel } from "./order.model";
 
+//** this function works but currently only the user object is shown in the get result, not the delivery option and products
+
+// get all orders
 export const getAllOrders = async (req: Request, res: Response) => {
   // TODO: who is allowed to use this endpoint?
   const orders = await OrderModel.find({}).populate<{ customer: User }>(
@@ -11,15 +14,14 @@ export const getAllOrders = async (req: Request, res: Response) => {
   res.status(200).json(orders);
 };
 
+// create a new order
 export const addOrder = async (
   req: Request<{}, {}, Order>,
   res: Response,
   next: NextFunction
 ) => {
-  // TODO: how do we handle errors in async middlewares?
   try {
-    const order = new OrderModel(req.body);
-    await order.save();
+    const order = await OrderModel.create(req.body);
     // console.log(order);
     // const errors = order.validateSync();
     res.status(200).json(order);
@@ -28,15 +30,20 @@ export const addOrder = async (
   }
 };
 
+// update an order (only isShipped)
 export const updateOrder = async (
   req: Request<{ id: string }>,
   res: Response
 ) => {
-  const order = await OrderModel.findById(req.params.id).select("+password");
-  console.log(order);
-  res.status(200).json("UPDATED order WITH ID :" + req.params.id);
-};
-
-export const deleteOrder = (req: Request, res: Response) => {
-  res.status(200).json("DELETED ORDER");
+  await OrderModel.findById(req.params.id);
+  try {
+    let { isShipped } = req.body;
+    let order = await OrderModel.findByIdAndUpdate(req.params.id, req.body);
+    if (isShipped) order!.isShipped = isShipped;
+    res.status(200).json("UPDATED ORDER WITH ID :" + req.params.id);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return res.status(500).json(err.message);
+    }
+  }
 };
