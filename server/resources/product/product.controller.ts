e778fadd066026a49a2ccbf6307ec8c0febcc00a
Millie Cheung <cheungmillie9@gmail.com>
2022-05-23 15:25:58 +0200

    adjusted error handling based on David's advice

--- server/resources/product/product.controller.ts
+++ server/resources/product/product.controller.ts
@@ -1,35 +1,30 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
+import { ErrorCodes } from "../../errorRequestHandler";
 import { Product, ProductModel } from "./product.model";
 require("express-async-errors");
 
 // get full product list from product db
 export const getAllProducts = async (req: Request, res: Response) => {
   const products = await ProductModel.find({});
-  if (!products) throw Error("not found");
+  if (!products) throw Error(ErrorCodes.notFound);
   res.status(200).json(products);
 };
 
 // get one specific product from product db
 export const getOneProduct = async (req: Request, res: Response) => {
   const product = await ProductModel.findById(req.params.id);
-  if (!product) throw Error("not found");
+  if (!product) throw Error(ErrorCodes.notFound);
   res.status(200).json(product);
 };
 
 // add a new product to product db
 export const addProduct = async (
   req: Request<{}, {}, Product>,
-  res: Response,
-  next: NextFunction
+  res: Response
 ) => {
-  try {
-    const product = await ProductModel.create(req.body);
-    // const errors = product.validateSync();
-    res.status(200).json(product);
-  } catch (err: any) {
-    if (err.code == 11000) throw Error("unauthorized_proudct");
-    throw Error("other");
-  }
+  const product = await ProductModel.create(req.body);
+  // const errors = product.validateSync();
+  res.status(200).json(product);
 };
 
 // update a product
@@ -37,13 +32,12 @@ export const updateProduct = async (
   req: Request<{ id: string }>,
   res: Response
 ) => {
-  const currentProduct = await ProductModel.findById(req.params.id);
   let { title, description, category, price, quantity, image } = req.body;
   const updatingProduct = await ProductModel.findByIdAndUpdate(
     req.params.id,
     req.body
   );
-  if (!updatingProduct) throw Error("not found");
+  if (!updatingProduct) throw Error(ErrorCodes.notFound);
   // MongooseError.StrictModeError ??
   if (title) updatingProduct!.title = title;
   if (description) updatingProduct!.description = description;
@@ -57,6 +51,6 @@ export const updateProduct = async (
 // delete a product from the product db
 export const deleteProduct = async (req: Request, res: Response) => {
   const product = await ProductModel.findByIdAndDelete(req.params.id);
-  if (!product) throw Error("not found");
+  if (!product) throw Error(ErrorCodes.notFound);
   res.status(200).json("Deleted product :" + req.params.id);
 };
